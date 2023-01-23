7585368b92a6e468fc0afcf697d8e74cd5719a69
Diana-Mel <117311842+Diana-Mel@users.noreply.github.com>
2023-01-23 22:49:11 +0000

    data stored to the local storage

--- script.js
+++ script.js
@@ -10,19 +10,12 @@
 
 // 2. Colour code each block based on the current time
 //      Create variables to target each time block
-    var hour09 = $('#9am')
-    var hour10 = $('#10am')
-    var hour11 = $('#11am')
-    var hour12 = $('#12pm')
-    var hour13 = $('#1pm')
-    var hour14 = $('#2pm')
-    var hour15 = $('#3pm')
-    var hour16 = $('#4pm')
-    var hour17 = $('#5pm')
-    var hour18 = $('#6pm')
+var saveButton = $('.saveBtn')
+var blockEl = $('.row')
+var textArea = $('.description')
 
 //      In the html add the data-hour which represent which hour each element is
-    textArea = $('.description')
+    
     textArea.each(function() {
         hour = $(this).attr('data-hour');
         console.log(hour)
@@ -39,51 +32,61 @@
     })
 
 // 3. Save input to local storage
-//      create variable (an array) called inputs that will store all of the input data
-    
-   var saveButton = $('.saveBtn')
-   var time = $('.hour')
-    console.log(time)
 
-//      Add event listener to all save button
+//  This function attackes a click event on each button.     
     saveButton.on('click', function(event){
 
-//      Add event.preventDefault inside the click event listener
+//  Stops the page from reloading      
+    event.preventDefault();
+
+//  Selects the element with the class decription  
+    var userInput = $(this).siblings(".description").val();
+//  Selects the parent element with its id attribute     
+    var hourSaved = $(this).parent().attr("id");
+
+//   
+    var input = []
+//  Takes the data already logged in the local storage
+    var alreadyInStorage = localStorage.getItem("User schedule")
+    
+//  If statement that checks if there is any data in the storage and stores it in variable. 
+    if (alreadyInStorage !== null) {
+      input = JSON.parse(alreadyInStorage);
+    }
+//  Loop that checks if old data matches new data and updates it    
+    for (i = 0; i < input.length; i++) {
+        if (input[i].time === hourSaved) {
+        input[i].todo = userInput;
+        localStorage.setItem("User schedule", JSON.stringify(input));
+        return;
+        }
+    }
+//  Creates an object array   
+    input.push({ time: hourSaved, todo: userInput })
+//    Stores input in the local storage    
+    localStorage.setItem("User schedule", JSON.stringify(input));
 
 })
 
-  
-//          Push the input value to inputs array with the format of { time: xx, input: xxx }
-//              Get the input value
-//              Get the hour value
-//              Push to the inputs array if the hour entry not exist yet in the array
-//              Replace the existing entry if the hour entry exist in the array
-//          Save inputs variable to local storage
-//              Stringify the inputs array
-//          Show feedback message to the user (optional)
-//          The feedback need to be dissappeared automatically
-//
-//      Example of inputs array format
-//      var inputs = [{
-//        time: 9,
-//        input: 'Meeting',
-//      },{
-//        time: 10,
-//        input: 'Coffee',
-//      },...];
 // 4. Load input from local storage when page load/refresh if there's any data in local storage
-//      var localStorageInput = get data from local storage
-//      check if localStorageInput exist, if it is
-//          var parsedLocalStorageInput = parse localStorageInput
-//          populate the inputs with the value from parsedLocalStorageInput
-//          inputs.forEach(function(input) {
-//              input => {
-//                time: 9,
-//                input: 'Meeting',
-//              }
-//              $('.timeblock[data-hour="' + input.time + '"] textarea').val(input.input)
-//          })
-//      if theres no data in local storage, do nothing
-
-// Extra, add hover effect on the save button
-
+      function inputLocalStorage() {
+        var alreadySaved = localStorage.getItem("user schedule")
+//        
+        if (alreadySaved !== null) {
+          alreadySaved = JSON.parse(alreadySaved);
+        } else {
+          return;
+        };
+        
+        for (i = 0; i < blockEl.length; i++) {
+            var hourFromId = blockEl[i].getAttribute("id");
+            for (j = 0; j < alreadySaved.length; j++) {
+              var timeSaved = alreadySaved[j].time;
+              var todoSaved = alreadySaved[j].todo;
+              if (hourFromId === timeSaved) {
+                blockEl[i].children[1].value = todoSaved;
+              }
+            }
+        }
+    }
+    inputLocalStorage()
